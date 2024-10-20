f221c3c569b03323f9cd524c7d2dde87fbf7aa81
Alan Nicolas <alanicolax@gmail.com>
2024-10-20 01:32:48 -0400

    Criçãp do componente Acesse, adicionei as imagens

---
+++ src/componentes/Register/componentes/Acesse.jsx
@@ -0,0 +1,17 @@
+import "./Acesse.css";
+
+function Acesse() {
+  return (
+    <div className="borda">
+      <div className="image-container">
+        <img src="./src/componentes/Register/images/google 1.svg" alt="" />
+
+        <img src="./src/componentes/Register/images/linkdin.svg" alt="" />
+
+        <img src="./src/componentes/Register/images/facebook 1.svg" alt="" />
+      </div>
+    </div>
+  );
+}
+
+export default Acesse;
