6a974ab77e1f541840f28caafd26cf0a8da499b8
Alan Nicolas <alanicolax@gmail.com>
2024-11-03 00:56:49 -0400

    Criação auth e valores para o firestore e validação senhas

--- src/componentes/Register/Register.jsx
+++ src/componentes/Register/Register.jsx
@@ -1,37 +1,119 @@
 import "./Register.css";
+
 import Acesse from "./componentes/Acesse.jsx";
-function Register() {
+import { auth, db } from "./Config/firebase.js";
+import { useState } from "react";
+import "firebase/auth";
+import { addDoc, collection } from "firebase/firestore";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+
+const Register = () => {
+  const [fullname, setFullname] = useState("");
+  const [numberTel, setNumberTel] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordConfirm, setPasswordConfirm] = useState("");
+
+  async function handleForm(e) {
+    e.preventDefault();
+    //validação que retorna um alert na tela com a mensagem da restrição
+    if (!validarSenha(password)) {
+      alert(
+        "A senha deve conter no mínimo 8 caracteres, incluindo um número, uma letra maiúscula, uma letra minúscula e um caractere especial."
+      );
+      return;
+    }
+    //jogar os valores dos inputs para o firetore database, estrutura dentro de uma função.
+    await addDoc(collection(db, "usuarios"), {
+      nome_completo: fullname,
+      numero_telefone: numberTel,
+      email: email,
+      senha: password,
+    });
+    alert("cadastro realizado!");
+    setFullname("");
+    setNumberTel("");
+    setEmail("");
+    setPassword("");
+    setPasswordConfirm("");
+  }
+
+  //função assincrona que ao clicar no botao o usuario tem a sua autenticação de email e senha dentro do firebase
+  async function cadastrar() {
+    await createUserWithEmailAndPassword(auth, email, password);
+  }
+
+  // uma constante function que restrige a senha com o formato default
+  const validarSenha = (password) => {
+    const regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).{8,}$/;
+
+    return regex.test(password);
+  };
+
   return (
     <div className="Registrar">
       <h1 className="titulo">
+        <link rel="" href="" />
         Criar uma conta. Vamos-lá! <br />
         <span className="menor">
           Já é membro?<span className="Link"> Faça login</span>
         </span>
       </h1>
 
-      <div className="forms">
+      <form className="forms" onSubmit={handleForm}>
         <input
           type="text"
           placeholder="Nome Completo:"
           name="Nome Completo"
+          id="fullNameInput"
+          onChange={(e) => setFullname(e.target.value)}
+          value={fullname}
           required
         />
         <input
-          type="tel"
+          type="text"
           placeholder="Numero de telefone:"
           name="numero"
+          id="telNumberInput"
+          onChange={(e) => setNumberTel(e.target.value)}
+          value={numberTel}
           required
         />
-        <input type="email" placeholder="E-mail:" name="email" required />
-        <input type="password" placeholder="Senha:" name="senha" required />
+        <input
+          type="email"
+          placeholder="E-mail:"
+          name="email"
+          id="emailInput"
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+          required
+        />
+        <input
+          type="password"
+          placeholder="Senha:"
+          name="senha"
+          id="passwordInput"
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+          required
+        />
+
         <input
           type="password"
           placeholder="Confirmar senha:"
           name="confirmar senha"
+          id="confPasswordInput"
+          onChange={(e) => setPasswordConfirm(e.target.value)}
+          value={passwordConfirm}
           required
         />
-        <button className="createAccount" type="submit">
+
+        <button
+          className="createAccount"
+          type="submit"
+          id="buttonCreateAccount"
+          onClick={cadastrar}
+        >
           Criar Conta
         </button>
 
@@ -42,9 +124,9 @@ function Register() {
           </span>
           <div className="linha"></div>
         </div>
-      </div>
+      </form>
       <Acesse />
     </div>
   );
-}
+};
 export default Register;
