e1ccb86cdf9834728c327bee794f1a34876d68c8
Alan Nicolas <alanicolax@gmail.com>
2024-11-07 19:07:47 -0400

    adicionei eventos ao clicar para redirecionar para ambas pages de login

--- src/componentes/Register/componentes/Acesse.jsx
+++ src/componentes/Register/componentes/Acesse.jsx
@@ -1,14 +1,24 @@
+import { signInWithFacebook, signInWithGoogle } from "../Config/firebase";
 import "./Acesse.css";
 
 function Acesse() {
   return (
     <div className="borda">
       <div className="image-container">
-        <img src="./src/componentes/Register/images/google 1.svg" alt="" />
+        <img
+          src="./src/componentes/Register/images/google 1.svg"
+          onClick={signInWithGoogle}
+          style={{ cursor: "pointer" }}
+        />
 
         <img src="./src/componentes/Register/images/linkdin.svg" alt="" />
 
-        <img src="./src/componentes/Register/images/facebook 1.svg" alt="" />
+        <img
+          src="./src/componentes/Register/images/facebook 1.svg"
+          alt=""
+          onClick={signInWithFacebook}
+          style={{ cursor: "pointer" }}
+        />
       </div>
     </div>
   );
