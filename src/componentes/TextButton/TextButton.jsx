75177a8d74a534e6f876f50fae65bec2a1af01d5
Alan Nicolas <alanicolax@gmail.com>
2024-11-11 18:53:18 -0400

    estrutura do componente

--- src/componentes/TextButton/TextButton.jsx
+++ src/componentes/TextButton/TextButton.jsx
@@ -1,7 +1,28 @@
 import "./TextButton.css";
+import { Link, useNavigate } from "react-router-dom";
 
 function TextButton() {
-  return <div className="text"></div>;
+  const navigate = useNavigate();
+
+  function nav() {
+    return navigate("/Pagina-Inicial");
+  }
+  return (
+    <div className="text">
+      <h1>
+        Oops! Esta página não pode ser encontrada! <br />
+        <span>
+          Parece que não conseguimos encontrar o que você está procurando.
+          retorne para a página principal.
+        </span>
+      </h1>
+      <button type="submit" onClick={nav}>
+        <Link to={"/Login"}>
+          <img src="../src\assets\Retornar a página principal.png" alt="" />
+        </Link>
+      </button>
+    </div>
+  );
 }
 
 export default TextButton;
