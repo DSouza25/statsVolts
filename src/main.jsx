5c223e1b2adaa868d0023a5e36e36e8ee5c13500
Alan Nicolas <alanicolax@gmail.com>
2024-11-15 17:50:06 -0400

    page carremento e configs

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,7 @@ import Login from "./pages/Login";
 import App from "./App";
 import ErrorPage from "./pages/ErrorPage";
 import PageHome from "./pages/PageHome";
+import Carregamento from "./pages/Carregamento";
 
 //config do router
 
@@ -22,6 +23,10 @@ const router = createBrowserRouter([
   {
     path: "/Pagina-Inicial",
     element: <PageHome />,
+  },
+  {
+    path: "/car",
+    element: <Carregamento />,
   },
 ]);
 
--- src/pages/Carregamento.jsx
+++ src/pages/Carregamento.jsx
@@ -1,5 +1,12 @@
+import Carregar from "../componentes/Carregamento/Carregar/Carregar";
+import "/src/csspages/Carregamento.css";
+
 function Carregamento() {
-  return <div className="body"></div>;
+  return (
+    <div className="body">
+      <Carregar />
+    </div>
+  );
 }
 
 export default Carregamento;
