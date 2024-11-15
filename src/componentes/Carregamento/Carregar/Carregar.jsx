66242c5c15ff429d583572846d5ae63b1e048769
Alan Nicolas <alanicolax@gmail.com>
2024-11-15 17:49:23 -0400

    posicionamento logo e tite

---
+++ src/componentes/Carregamento/Carregar/Carregar.jsx
@@ -0,0 +1,13 @@
+import "./Carregar.css";
+
+function Carregar() {
+  return (
+    <div className="bordaMain">
+      <img src="src\assets\logo-stats-volts.svg" className="logo" alt="" />
+      <h1 className="nomeApp">STATSVOLTS</h1>
+      <span className="carregar"></span>
+    </div>
+  );
+}
+
+export default Carregar;
