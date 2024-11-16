9516edf5111162ddcf11f37be2973384a3f87a43
Alan Nicolas <alanicolax@gmail.com>
2024-11-16 16:29:34 -0400

    ALTERAÇÕES

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,6 @@ import Login from "./pages/Login";
 import App from "./App";
 import ErrorPage from "./pages/ErrorPage";
 import PageHome from "./pages/PageHome";
-import Carregamento from "./pages/Carregamento";
 
 //config do router
 
@@ -23,10 +22,6 @@ const router = createBrowserRouter([
   {
     path: "/Pagina-Inicial",
     element: <PageHome />,
-  },
-  {
-    path: "/car",
-    element: <Carregamento />,
   },
 ]);
 
