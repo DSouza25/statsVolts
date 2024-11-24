c0100d2347be0500e9fea3ebed8ffed21a80b508
Alan Nicolas <alanicolax@gmail.com>
2024-11-24 16:56:30 -0400

    login configs

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,38 @@
 import "../csspages/LoginPage.css";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { useSignInWithEmailAndPassword } from "react-firebase-hooks/auth";
+import { auth } from "../componentes/Register/Config/firebase";
+import { useState } from "react";
+
+import { useEffect } from "react";
 
 function Login() {
+  const [SignInWithEmailAndPassword, user, loading, error] =
+    useSignInWithEmailAndPassword(auth);
+
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+
+  const navigate = useNavigate();
+
+  function handleSignIn(e) {
+    e.preventDefault();
+    SignInWithEmailAndPassword(email, password);
+  }
+
+  useEffect(() => {
+    if (user) {
+      alert("conta confirmada");
+      navigate("/dashboard");
+    }
+  }); // Redireciona para a página de dashboard } }, [user, navigate]);
+
+  useEffect(() => {
+    if (error) {
+      alert("Conta Inválida, Verifique email e senha.");
+    }
+  }, [error]);
+
   return (
     <div className="login-container">
       <div className="login-header">
@@ -14,6 +45,7 @@ function Login() {
           placeholder="E-mail:"
           name="email"
           id="emailInput"
+          onChange={(e) => setEmail(e.target.value)}
           required
         />
         <input
@@ -21,10 +53,11 @@ function Login() {
           placeholder="Senha:"
           name="senha"
           id="passwordInput"
+          onChange={(e) => setPassword(e.target.value)}
           required
         />
 
-        <button type="submit" className="login-button">
+        <button type="submit" className="login-button" onClick={handleSignIn}>
           LOGIN
         </button>
       </form>
