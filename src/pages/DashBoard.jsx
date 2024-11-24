e8cac6b64f3af8df132aa6d04c3e602db9047528
Vinicius-the-Mirio <vinicius14martins14@gmail.com>
2024-11-24 17:33:44 -0300

    Gráficos de destaques

--- src/pages/DashBoard.jsx
+++ src/pages/DashBoard.jsx
@@ -1,12 +1,14 @@
 import Donut from "../componentes/Graficos/Consumo_diario";
 import Grafico_linha from "../componentes/Graficos/Consumo_semanal";
 import Consumo_mensal from "../componentes/Graficos/Consumo_mensal";
+import Destaques from "../componentes/Graficos/Destaques";
 function Dashboard() {
   return (
   <div> 
     <Consumo_mensal/>
     <Grafico_linha/>
-  <Donut/>
+ 
+  <Destaques/>
 
 
   </div>
