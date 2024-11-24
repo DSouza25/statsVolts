1588401998ef46a65b92a5549ae2145d90dc407e
Alan Nicolas <alanicolax@gmail.com>
2024-11-23 23:10:59 -0400

    criação do menu e alterações e configs no layout

--- src/componentes/Menu/Menu.jsx
+++ src/componentes/Menu/Menu.jsx
@@ -1,7 +1,36 @@
+import { Link } from "react-router-dom";
 import "./Menu.css";
+import {menu} from "../../dados"
 
 function Menu() {
-  return <div className="Menu">Menu</div>;
+  return (
+    <div className="Menu">
+    {menu.map((item) =>(
+    <div className="item">
+    <span className="titulo-menu">MENU</span>
+    <Link to={"/"} className="listItem">
+      <img src="src\assets\Chart.svg" alt="" />
+      <span className="listItemTitle">DashBoard</span>
+    </Link>
+    <Link to={"/"} className="listItem">
+      <img src="src\assets\Document.svg" alt="" />
+      <span className="listItemTitle">Gerenciar Menu</span>
+    </Link>
+  </div>
+  <div className="item">
+    <span className="titulo-menu">MENU</span>
+    <Link to={"/"} className="listItem">
+      <img src="src\assets\Chart.svg" alt="" />
+      <span className="listItemTitle">DashBoard</span>
+    </Link>
+    <Link to={"/"} className="listItem">
+      <img src="src\assets\Document.svg" alt="" />
+      <span className="listItemTitle">Gerenciar Menu</span>
+    </Link>
+  </div>
+    ))}  
+    </div>
+  );
 }
 
 export default Menu;
