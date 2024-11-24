dfc2e62df997c7d596af516efad1af642df33f67
Alan Nicolas <alanicolax@gmail.com>
2024-11-23 23:26:10 -0400

    configs menu

--- src/componentes/Menu/Menu.jsx
+++ src/componentes/Menu/Menu.jsx
@@ -1,34 +1,21 @@
 import { Link } from "react-router-dom";
 import "./Menu.css";
-import {menu} from "../../dados"
+import { menu } from "../../dados";
 
 function Menu() {
   return (
     <div className="Menu">
-    {menu.map((item) =>(
-    <div className="item">
-    <span className="titulo-menu">MENU</span>
-    <Link to={"/"} className="listItem">
-      <img src="src\assets\Chart.svg" alt="" />
-      <span className="listItemTitle">DashBoard</span>
-    </Link>
-    <Link to={"/"} className="listItem">
-      <img src="src\assets\Document.svg" alt="" />
-      <span className="listItemTitle">Gerenciar Menu</span>
-    </Link>
-  </div>
-  <div className="item">
-    <span className="titulo-menu">MENU</span>
-    <Link to={"/"} className="listItem">
-      <img src="src\assets\Chart.svg" alt="" />
-      <span className="listItemTitle">DashBoard</span>
-    </Link>
-    <Link to={"/"} className="listItem">
-      <img src="src\assets\Document.svg" alt="" />
-      <span className="listItemTitle">Gerenciar Menu</span>
-    </Link>
-  </div>
-    ))}  
+      {menu.map((item) => (
+        <div className="item" key={item.id}>
+          <span className="titulo-menu">{item.title}</span>
+          {item.listItems.map((listItems) => (
+            <Link to={"/"} className="listItem" key={listItems.id}>
+              <img src={listItems.icon} alt="" />
+              <span className="listItemTitle">{listItems.title}</span>
+            </Link>
+          ))}
+        </div>
+      ))}
     </div>
   );
 }
